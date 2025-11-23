50e515101cc436dc918b2d762da85ff82ea46d92
Jeevan-LP <jeevangowda016@gmail.com>
2025-11-23 11:54:20 +0530

    Latest committed?

--- src/test/java/ActionClass/Sliders.java
+++ src/test/java/ActionClass/Sliders.java
@@ -1,46 +1,30 @@
 package ActionClass;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class Sliders {
 
-	public static void main(String[] args) throws Throwable {
+    @Test
+    public void testSlider() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.jqueryscript.net/demo/Price-Range-Slider-jQuery-UI/");
 
-		WebDriver BrowserDriver=new ChromeDriver();
-		System.out.println("Browser is launching");
-
-		BrowserDriver.get("https://www.jqueryscript.net/demo/Price-Range-Slider-jQuery-UI/");
-		BrowserDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		BrowserDriver.manage().window().maximize();
-		
-		Thread.sleep(2000);
-		WebElement slider = BrowserDriver.findElement(By.xpath("//div[@class='ui-slider-range ui-corner-all ui-widget-header']"));
-		System.out.println("X axis = "+slider.getRect().getX());
-		System.out.println("Y axis = "+slider.getRect().getY());
-
-		Actions action = new Actions(BrowserDriver);
-		WebElement startSlider = BrowserDriver.findElement(By.xpath("//div[@id='slider-range']//span[1]"));
-		WebElement endSlider = BrowserDriver.findElement(By.xpath("//div[@id='slider-range']//span[2]"));
-		action.dragAndDropBy(startSlider, 80, 0).perform();
-		Thread.sleep(2000);
-		action.dragAndDropBy(endSlider, -50, 0).perform();
-		
-		System.out.println("after slider drag X axis = "+slider.getRect().getX());
-		System.out.println("after slider drag Y axis = "+slider.getRect().getY());
+        WebElement min = driver.findElement(By.xpath("//div[@id='slider-range']//span[1]"));
+        WebElement max = driver.findElement(By.xpath("//div[@id='slider-range']//span[2]"));
+        new Actions(driver).dragAndDropBy(min, 80, 0).perform();
+        Thread.sleep(1000);
+        System.out.println("Y axis------"+driver.manage().window().getSize().height);
+        new Actions(driver).dragAndDropBy(max, -50, 0).perform();
+        Thread.sleep(1000);
+        System.out.println("X axis------"+driver.manage().window().getSize().width);
 
 
-		Thread.sleep(5000);
-		BrowserDriver.quit();
-
-
-		
-
-	}
-
+        driver.quit();
+    }
 }
